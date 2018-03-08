395cfcd4b8031635fb983680a1310d4a2deb993a
Daniel <dds2130@rit.edu>
2018-03-07 22:00:59 -0500

    reading pdf

--- js/rules.js
+++ js/rules.js
@@ -1,4 +1,4 @@
-const PdfReader = require('PdfReader');
+var pdfjsLib = require('pdfjs-dist');
 const {reload, rndrsp} = require('./shared.js');
 
 module.exports = function(rule) {
@@ -7,16 +7,57 @@ module.exports = function(rule) {
 
 	if (!rule) return rndrsp(sass["!providerule"]);
 
-	// if (rules.hasOwnProperty(rule)) return `${rules[rule]}`;
+	if (rules.hasOwnProperty(rule)) return `${rules[rule]}`;
 
-	// return rndrsp(sass["!norule"]);
+	pdfjsLib.getDocument("./ComprehensiveRules.pdf").then((doc) => {
+	  var numPages = doc.numPages;
+	  // console.log('# Document Loaded');
+	  // console.log('Number of Pages: ' + numPages);
+	  // console.log();
 
-	new PdfReader().parseFileItems("../ComprehensiveRules.pdf", function(err, item){
-	  if (err)
-	    callback(err);
-	  else if (!item)
-	    callback();
-	  else if (item.text)
-	    console.log(item.text);
+	  var lastPromise; // will be used to chain promises
+	  lastPromise = doc.getMetadata().then(function (data) {
+	    console.log('# Metadata Is Loaded');
+	    console.log('## Info');
+	    console.log(JSON.stringify(data.info, null, 2));
+	    console.log();
+	    if (data.metadata) {
+	      console.log('## Metadata');
+	      console.log(JSON.stringify(data.metadata.getAll(), null, 2));
+	      console.log();
+	    }
+	  });
+
+	  var loadPage = function (pageNum) {
+	    return doc.getPage(pageNum).then(function(page) {
+	      console.log('# Page ' + pageNum);
+	      // var viewport = page.getViewport(1.0 /* scale */);
+	      // console.log('Size: ' + viewport.width + 'x' + viewport.height);
+	      // console.log();
+	      return page.getTextContent().then(function(content) {
+	        // Content contains lots of informationabout the text layout and
+	        // styles, but we need only strings at the moment
+	        var strings = content.items.map(function(item) {
+	          return item.str;
+	        });
+	        // console.log('## Text Content');
+	        console.log(strings.join(' '));
+	      }).then(function() {
+	        console.log();
+	      });
+	    })
+	  };
+	  // Loading of the first page will wait on metadata and subsequent loadings
+	  // will wait on the previous pages.
+	  for (var i = 1; i <= numPages; i++) {
+	    lastPromise = lastPromise.then(loadPage.bind(null, i));
+	  }
+	  return lastPromise;
+	}).then(function() {
+	  // console.log('# End of Document');
+	}, function(err) {
+	  console.error('Error: ' + err);
 	});
+
+	return rndrsp(sass["!norule"]);
 }
