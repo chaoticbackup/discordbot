56b92bebd9f99fa292eac925b6987bb6bdbc17d8
Daniel <dds2130@rit.edu>
2018-07-20 20:23:38 -0400

    removed log

--- src/bot.js
+++ src/bot.js
@@ -25,8 +25,7 @@ bot.on('ready', function (evt) {
 // Automatically reconnect if the bot disconnects due to inactivity
 bot.on('disconnect', (erMsg, code) => {
 	logger.info('Reconnecting');
-	console.log(this);
-    bot.login(auth.token);
+    // bot.login(auth.token);
 });
 
 // Respones
