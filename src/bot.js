8def45837e6239b6ab0f8e4e72c7ccacd54ea675
Daniel <dds2130@rit.edu>
2018-10-22 15:34:40 -0400

    delete spam bot accounts

--- src/bot.js
+++ src/bot.js
@@ -25,11 +25,24 @@ bot.on('ready', function (evt) {
 // Automatically reconnect if the bot disconnects due to inactivity
 bot.on('disconnect', (erMsg, code) => {
 	logger.info('Reconnecting');
-    bot.login(auth.token);
+  bot.login(auth.token);
 });
 
 // Respones
 bot.on('message', responses.bind(bot));
+
+// Ban Spam
+bot.on('guildMemberAdd', (member) => {
+	if (member.displayName.match(new RegExp("(discord\.me)|(discord\.gg)|(bit\.ly)|(twitch\.tv)", "i"))) {
+		if (member.bannable) {
+			member.ban(); 
+			logger.info('Banned: ' + member.displayName);
+			// Delete the welcome message
+			let meebot = bot.users.find(user => user.id == 159985870458322944);
+			if (meebot && meebot.lastMessage && meebot.lastMessage.deletable) meebot.lastMessage.delete();
+		}
+	}
+});
 
 /* LOGIN */
 bot.login(auth.token);
