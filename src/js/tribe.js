c57d968e97e9424885d77cc940b77efa951e3a02
Daniel <dds2130@rit.edu>
2019-06-09 17:09:04 -0400

    Added custom responses

--- src/js/tribe.js
+++ src/js/tribe.js
@@ -10,29 +10,37 @@ export function setTribe(tribe, message, bot) {
         }
     });
 
+    let response = ""
     switch(tribe.toLowerCase()) {
         case 'danian':
             tribe = "Danian";
+            response = `<:gottahave:400174328215502851>: Yo, you're one of the hive now.`;
             break;
         case 'mipedian':
             tribe = "Mipedian";
+            response = `<:Shim:315235831927537664>: What's up my dude? heh heh heh, welcome to the fun.`;
             break;
         case 'marrillian':
         case "m'arrillian":
             tribe = "M'arrillian";
+            response = `<:Mar:294942283273601044>: You'll serve you're purpose.`
             break;
         case 'overworld':
             tribe = "OverWorld";
+            response = `<:Bodal:401553896108982282>: You have joined the mighty forces of the OverWorld.`;
             break;
         case 'underworld':
             tribe = "UnderWorld";
+            response = `<:Chaor:285620681163669506>: Puny humans can still fight for Chaor!`;
             break;
         case 'tribeless':
         case 'generic':
             tribe = "Tribeless";
+            response = `<:creepy:471863166737973268>: 👀`;
             break;
         case 'frozen':
             tribe = "Frozen";
+            response = `Shhhh we haven't been revealed yet`;
             break;
         default:
             return `${tribe} is not a valid faction`;
@@ -41,10 +49,7 @@ export function setTribe(tribe, message, bot) {
     let guild_role = bot.guilds.get(message.guild.id).roles.find(role => role.name===tribe);
     if (guild_role) {
         message.member.addRole(guild_role);
-        if (tribe = "Frozen")
-            return `Shhhh we haven't been revealed yet`;
-        else
-            return `You have joined the ${tribe}`;
+        return response;
     }
 
     return `Sorry this guild doesn't have tribal roles`;
