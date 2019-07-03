f0fe28caef34f4313946979abfdfaa4ca7d4c672
Daniel <dds2130@rit.edu>
2019-07-03 01:18:05 -0400

    updated tierlist

--- src/js/meta.js
+++ src/js/meta.js
@@ -1,7 +1,7 @@
 const { RichEmbed } = require('discord.js');
 
 export function tierlist() {
-	return new RichEmbed().setImage('https://drive.google.com/uc?id=1kQFLuJu5NZgpa3WLH1f0PtkSKDHWzcj8');
+	return new RichEmbed().setImage('https://drive.google.com/uc?id=1h9QOd2sk1KD4WK91FLy5CQPcar4twGlA');
 }
 export function tierlisttext() {
     const {tierlist, decks} = require('../config/tierlist.json');
