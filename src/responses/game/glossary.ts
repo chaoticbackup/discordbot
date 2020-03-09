8a0d33fae1ca85f7e0a3363e60082d3c25e63357
Daniel <dds2130@rit.edu>
2020-03-09 13:37:39 -0400

    glossary terms automatically show definition

--- src/responses/game/glossary.ts
+++ src/responses/game/glossary.ts
@@ -2,11 +2,18 @@ import { cleantext } from '../../common';
 const glossary = require('../config/glossary');
 
 export default function (rule: string) {
-  rule = rule.replace(/^(at )*(the )/i, '');
+  rule = rule.replace(/^(at )*(the )/i, '')
+    .replace(/(abilities)$/i, 'ability')
+    .replace(/(creatures)/i, 'creature');
 
-  for (var key in glossary) {
-    if (cleantext(key).indexOf(cleantext(rule)) === 0)
-    { return `*${key}*:\n${glossary[key]}`; }
+  for (const key in glossary) {
+    if (cleantext(key).indexOf(cleantext(rule)) === 0) {
+      let value = glossary[key];
+      if (value.indexOf('See') === 0) {
+        value = glossary[value.match(/See \"(.*)\"/i)[1]];
+      }
+      return `*${key}*:\n${value}`;
+    }
   }
 
   return 'I\'m not sure, but you can check the Player Guide:\n' +
