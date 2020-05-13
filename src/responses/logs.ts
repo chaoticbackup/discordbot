577c3d28bb7a721b9327e397dc02264f9da95a16
Daniel <dds2130@rit.edu>
2020-05-13 16:08:06 -0400

    filter out control character as well

--- src/responses/logs.ts
+++ src/responses/logs.ts
@@ -6,7 +6,7 @@ const home_path = path.resolve(__dirname, '..', '..');
 
 export default (message: Message) => {
   try {
-    const text = fs.readFileSync(path.resolve(home_path, 'out.log'), { encoding: 'utf8' }).toString().replace('[32m', '');
+    const text = fs.readFileSync(path.resolve(home_path, 'out.log'), { encoding: 'utf8' }).toString().replace(/\\[3[29]m)/g, '');
     if (text.length > 2000) {
       return text.slice(-2000);
     }
