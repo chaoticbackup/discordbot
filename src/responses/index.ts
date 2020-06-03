7e69441c93f333ce8a704d55c7e8794d309d64d6
Daniel <dds2130@rit.edu>
2020-06-02 21:18:01 -0400

    retry

--- src/responses/index.ts
+++ src/responses/index.ts
@@ -554,8 +554,8 @@ async function clear(amount: number, message: Message, mentions: string[] = []):
 }
 
 function haxxor(message: Message, bot: Client): void {
-  if ((message?.member.id === users('daddy') || message?.member.id === users('bf'))
-    || (message?.guild.id === servers('main').id && isModerator(message.member))
+  if ((message.member?.id === users('daddy') || message.member?.id === users('bf'))
+    || (message.guild?.id === servers('main').id && isModerator(message.member))
   ) {
     message.channel.send('Resetting...');
     API.rebuild()
