bc8322f7946ea37ed80c59490edefcf1022440ff
Daniel <dds2130@rit.edu>
2024-10-26 12:46:01 -0400

    fix wording

--- src/responses/sass.ts
+++ src/responses/sass.ts
@@ -22,7 +22,7 @@ export default async function (bot: Client, message: Message, mentions: string[]
   // coming back
   const back_regex = /((.*(chaotic).*(return|(com|be).*(back)).*)|(.*news.*(reboot|rebirth).*)|(.*(announcement|update).*chaotic.*))\?/i;
   if (content.match(back_regex)) {
-    const response = "While Chaotic's return has been officially confirmed, you'll be the first to know when it is. (We will make an announcement and ping everyone when they do.)";
+    const response = "While Chaotic's return has not been officially confirmed, you'll be the first to know when we get news. (We will make an announcement and ping everyone when they do.)";
     const m: Message = await send(response).catch(msgCatch);
     await m.react('586395473716445184').catch(msgCatch);
     return;
