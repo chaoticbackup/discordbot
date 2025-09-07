ce4bab8241add969dcda549681eff0217eb790fc
Daniel <dds2130@rit.edu>
2025-09-07 15:14:17 -0400

    consolidate code

--- src/responses/game/bans.ts
+++ src/responses/game/bans.ts
@@ -22,16 +22,19 @@ export { f as formats };
 export function banlist_update(message: Message) {
   const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'short', day: 'numeric' };
   const date = message.createdAt.toLocaleDateString('en-US', options);
-  let response = `**Standard (${date})**\n==**Banned Cards**==`;
-  response += (ban_lists.standard.ban).reduce((prev, curr) => `${prev}\n${curr}`, '');
-  response += '\n==**Unique Cards**==';
-  response += (ban_lists.standard.unique).reduce((prev, curr) => `${prev}\n${curr}`, '');
-  response += '\n==**Legendary Removed Cards**==';
-  response += (ban_lists.standard.unlegendary).reduce((prev, curr) => `${prev}\n${curr}`, '');
-  response += '\n==**Loyal Cards**==';
-  response += (ban_lists.standard.loyal).reduce((prev, curr) => `${prev}\n${curr}`, '');
-  response += '\n==**Loyal Removed Cards**==';
-  response += (ban_lists.standard.unloyal).reduce((prev, curr) => `${prev}\n${curr}`, '');
+  let response = `**Standard (${date})**`;
+
+  const list_bans = (_format: keyof typeof formats, list: string, header = '\n==**Banned Cards**==') => {
+    response += header;
+    response = (ban_lists[_format][list] as string[]).reduce((prev, curr) => `${prev}\n${curr}`, response);
+  };
+
+  list_bans('standard', 'ban');
+  list_bans('standard', 'unique', '\n==**Unique Cards**==');
+  list_bans('standard', 'unlegendary', '\n==**Legendary Removed Cards**==');
+  list_bans('standard', 'loyal', '\n==**Loyal Cards**==');
+  list_bans('standard', 'unloyal', '\n==**Loyal Removed Cards**==');
+
   return response;
 }
 
@@ -58,6 +61,7 @@ export function banlist(message: Message, format: string, options: string[] = []
       title('standard');
       list_bans('standard', 'ban');
       list_bans('standard', 'unique', '\n==**Unique Cards**==');
+      list_bans('standard', 'unlegendary', '\n==**Legendary Removed Cards**==');
       list_bans('standard', 'loyal', '\n==**Loyal Cards**==');
       list_bans('standard', 'unloyal', '\n==**Loyal Removed Cards**==');
       response += '\n=====\nYou can ask why a card was banned with ``"!whyban <card name>"``';
